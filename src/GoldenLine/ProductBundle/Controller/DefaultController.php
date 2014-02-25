b5e45c4b8f3a343fd86c252bd0c238b9e29681e3
Sebastian Grodzicki <sebastian@grodzicki.pl>
2014-02-25 14:31:16 +0100

    ProductBundle structure

---
+++ src/GoldenLine/ProductBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GoldenLine\ProductBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('ProductBundle:Default:index.html.twig');
+    }
+}
---
+++ src/GoldenLine/ProductBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GoldenLine\ProductBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($crawler->filter('html:contains("It works!")')->count() > 0);
+    }
+}
